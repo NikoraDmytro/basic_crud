c9ad3fd7de40f9e3df896ca96624426c1d20776d
Dmytro <dimaniko04@ukr.net>
2022-09-17 13:19:25 +0300

    Added cors to backend. Implemented rough render of users stored in DB.

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
@@ -12,6 +13,7 @@ const connectionString = process.env.DB_CONNECTION;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRouter);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import styles from "App.module.scss";
+
+const url = "http://localhost:8080/users";
+
+interface User {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  residence: string;
+  dateOfBirthday: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await fetch(url);
+      const fetchedUsers = await res.json();
+
+      setUsers(fetchedUsers);
+    };
+
+    fetchUsers();
+  }, []);
+
+  const parseDateString = (dateString: string) => {
+    const date = new Date(dateString);
+
+    return date.toDateString();
+  };
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className={styles.mainBox}>
+      <h1>Users List:</h1>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>
+            <p>{user.firstName}</p>
+            <p>{user.lastName}</p>
+            <p>{user.residence}</p>
+            <p>{parseDateString(user.dateOfBirthday)}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
